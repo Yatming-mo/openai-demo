906eb89fec27d23caa94fc62132ddf1de321c25e
Yatming <moyiming@tungee.com>
2023-03-23 18:00:02 +0800

    update igonre

---
+++ apps/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    __init__.py.py
+    ~~~~~~~~~~~~~~~~~~~~~~~
+    Description of this file
+    
+    :author: yatming
+    :copyright: (c) 2023, Tungee
+    :date created: 2023/3/23
+    :python version: 3.6
+
+"""
